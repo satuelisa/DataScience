e4f6a6758d377a82ed31414a32916060534595ff
SatuElisa <elisa.schaeffer@gmail.com>
2019-12-02 10:40:55 -0600

    Simple check for negative comments

---
+++ bash/p12_7.sh
@@ -0,0 +1,9 @@
+echo INI
+grep " no " signif_ini.txt | wc -l
+grep " no " signif_ini.txt 
+echo MCU
+grep " no " signif_mcu.txt 
+grep " no " signif_mcu.txt | wc -l
+echo ORD
+grep " no " signif_ord.txt | wc -l
+grep " no " signif_ord.txt 
