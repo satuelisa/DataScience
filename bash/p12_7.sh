c9685eae2964d9f5c2d16435d3d04e9fb5105152
SatuElisa <elisa.schaeffer@gmail.com>
2019-12-02 10:43:23 -0600

    Counts by type

--- bash/p12_7.sh
+++ bash/p12_7.sh
@@ -1,9 +1,12 @@
 echo INI
 grep " no " signif_ini.txt | wc -l
 grep " no " signif_ini.txt 
+grep " no " signif_ini.txt | awk -F ',' '{print $4}' | sort | uniq -c
 echo MCU
 grep " no " signif_mcu.txt 
 grep " no " signif_mcu.txt | wc -l
+grep " no " signif_mcu.txt | awk -F ',' '{print $4}' | sort | uniq -c
 echo ORD
 grep " no " signif_ord.txt | wc -l
 grep " no " signif_ord.txt 
+grep " no " signif_ord.txt | awk -F ',' '{print $4}' | sort | uniq -c
